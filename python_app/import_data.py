1d367429998989fc88bea40f21606e6d00a4a704
YuiP0908 <21239hank@gmail.com>
2025-05-14 15:44:37 +0800

    change import_data.py

--- python_app/import_data.py
+++ python_app/import_data.py
@@ -1,2 +1,6 @@
-# TODO: 匯入 Titanic CSV 資料進 MySQL
-print("尚未實作 import_data.py，請開發者補上匯入邏輯")
+import pandas as pd
+from sqlalchemy import create_engine
+engine = create_engine('mysql+pymysql://root:root@mysql/titanic_db')
+data = pd.read_csv('./csv/titanic.csv')
+data.to_sql('passengers',engine,chunksize=100000,index=None)
+print('存入成功!')
